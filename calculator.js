702cc26c7dfe4ca3b6b5f58a8561957e2237c3fe
froggy <chrisdetmering@gmail.com>
2021-05-08 12:04:39 -0700

    code review completed

--- calculator.js
+++ calculator.js
@@ -1,87 +1,94 @@
-//uncaught syntax error - answer has already been declared
-const operatorSet = new Set(["+", "-", "x", "/", "="]);
-let expressionArray = [];
-let numString = "";
-let answer = 0;
-const buttons = document.querySelectorAll("button");
-const outputBox = document.getElementById("output-box");
+let firstOperand = ''; //3 
+let secondOperand = ''; //''
+let operation = ''; //+
 
-buttons.forEach(function (button) {
-  button.addEventListener("click", storeNum);
-});
+document.querySelectorAll('.number')
+  .forEach(numberButton => {
+    numberButton.addEventListener('click', () => {
+      const selectedNumber = numberButton.textContent;
 
-/*taskListItem.addEventListener("contextmenu", (e) => { 
-  e.preventDefault();*/
+      if (!isOperation()) {
+        firstOperand += selectedNumber;
+        setDisplay(firstOperand);
+      } else {
+        secondOperand += selectedNumber;
+        setDisplay(secondOperand);
+      }
 
-function storeNum(event) {
-  //event.preventDefault();
-  let n = event.target.value;
-  if (n === "C") {
-    n = "";
-    clearOutputBox();
-  }
+    })
+  })
 
-  if (!operatorSet.has(n)) {
-    numString = numString + n;
-    console.log("number: ", numString);
-    console.log(expressionArray);
-  }
 
-  if (operatorSet.has(n)) {
-    expressionArray.push(numString);
-    expressionArray.push(n);
-    numString = "";
-    console.log("operator: ", n);
-    console.log("expressionArray: ", expressionArray);
-  }
-  outputBox.value = numString;
-  if (typeof expressionArray[2] !== "undefined") {
-    lastExpression();
-  } else if (n == "=") {
-    lastExpression();
-  }
+document.querySelectorAll(".operator")
+  .forEach(operatorButton => {
+    operatorButton.addEventListener("click", () => {
+      const selectedOperation = operatorButton.textContent;
+
+      if (isFirstOperand()) {
+        operation = selectedOperation;
+      }
+
+    })
+  })
+
+document.getElementById("equals")
+  .addEventListener("click", e => {
+    if (isSecondOperand()) {
+      const result = calculate();
+      firstOperand = result;
+      secondOperand = '';
+      operation = '';
+      setDisplay(result);
+    }
+
+    //handle second operation click
+  })
+
+//Handle pressing decimal 
+
+//Helper Functions 
+function isOperation() {
+  return !!operation
 }
 
-function lastExpression() {
-  //check if expressionArray[1] = '=' and remove it if it is;
-  if (expressionArray[1] == "=") {
-    expressionArray.shift();
-  }
-
-  num1 = parseFloat(expressionArray[0]);
-  operator = expressionArray[1];
-  num2 = parseFloat(expressionArray[2]);
-  answer = calculate(num1, operator, num2);
-  outputBox.value = answer;
-  expressionArray.splice(0, 3);
-  console.log("expressionArray post splice: ", expressionArray);
-  expressionArray.unshift(answer);
-  console.log("expressionArray post push: ", expressionArray);
+function setDisplay(value) {
+  document.getElementById("output-box").value = value;
 }
 
-function calculate(num1, operator, num2) {
-  switch (operator) {
+function isFirstOperand() {
+  return !!firstOperand;
+}
+
+function isSecondOperand() {
+  return !!secondOperand;
+}
+
+function calculate() {
+  switch (operation) {
     case "+":
-      return num1 + num2;
-      break;
-
+      return add();
     case "-":
-      return num1 - num2;
-      break;
-
+      return subtract();
     case "x":
-      return num1 * num2;
-      break;
-
+      return multiply();
     case "/":
-      return num1 / num2;
-    default:
-      console.log("Something went wrong :-(");
+      return divide();
   }
 }
 
-function clearOutputBox() {
-  document.getElementById("output-box").value = "";
-  expressionArray = [];
-  numString = "";
+
+function add() {
+  return `${Number(firstOperand) + Number(secondOperand)}`;
+}
+
+function subtract() {
+  return `${Number(firstOperand) - Number(secondOperand)}`;
+}
+
+function multiply() {
+  return `${Number(firstOperand) * Number(secondOperand)}`;
+}
+
+function divide() {
+  return `${Number(firstOperand) / Number(secondOperand)}`;
 }
